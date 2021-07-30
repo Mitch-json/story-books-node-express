b22403ba6eaba5e8a0f2e4165a8c861d6ecd6708
Mitch-json <mitchjaga@gmail.com>
2021-07-30 21:46:53 +0300

    Third Commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb+srv://mitch123:mitch123@cluster0-mvefg.mongodb.net/stories?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
